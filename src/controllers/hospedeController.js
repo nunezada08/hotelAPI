a4b867fec939e49bb92ec025f7c18b75eec77151
julia-anjos968 <julia.r.anjos@aluno.senai.br>
2026-04-08 14:41:27 -0300

    feat: finaliza o swagger

--- src/controllers/hospedeController.js
+++ src/controllers/hospedeController.js
@@ -3,15 +3,22 @@ import HospedeModel from "../models/HospedeModel.js";
 /**
  * @typedef {object} reqBodyHospede
  * @property {string} nome.required
- * @property {string} .required
- * @property {string} localidade
+ * @property {string} email.required
+ * @property {string} telefone.required
+ * @property {string} cep.required
+ * @property {boolean} ativo.required
  */
 
 /**
+ * POST /api/hospedes
+ * @tags Hospedes
+ * @summary Cria um novo registro de hospede
+ * @description EndPoint responsável por cadastrar um novo hospede no sistema web.
+ * @param {reqBodyHospede} request.body.required
  *
- * @param {*} req
- * @param {*} res
- * @returns
+ * @return 201 - Hospede criado com sucesso
+ * @return 400 - Dados inválidos ou campos obrigatórios não informados
+ * @return 500 - Erro interno no servidor
  */
 
 export const criar = async (req, res) => {
@@ -22,7 +29,7 @@ export const criar = async (req, res) => {
         .json({ error: "Corpo da requisição vazio. Envie os dados!" });
     }
 
-    const { nome, email, telefone, cep, logradouro, bairro, localidade, uf, ativo } = req.body;
+    const { nome, email, telefone, cep, ativo } = req.body;
 
     if (!nome) {
       return res.status(400).json({ error: 'O campo "nome" é obrigatório!' });
@@ -32,30 +39,25 @@ export const criar = async (req, res) => {
       return res.status(400).json({ error: 'O campo "email" é obrigatório!' });
     }
 
-      if (!telefone) {
-          return res
-              .status(400)
-              .json({ error: 'O campo "telefone" é obrigatório!' });
-      }
-
-    if (ativo !== undefined && typeof ativo !== "boolean") {
+    if (!telefone) {
+      return res
+        .status(400)
+        .json({ error: 'O campo "telefone" é obrigatório!' });
+    }
+    if (!cep) {
+      return res.status(400).json({ error: 'O campo "cep" é obrigatório!' });
+    }
+    if (ativo === undefined && typeof ativo !== "boolean") {
       return res
         .status(400)
         .json({ error: 'O campo "ativo" deve ser boolean!' });
     }
 
     const hospede = new HospedeModel({
-        nome,
-        email,
-        telefone,
-        cep,
-        logradouro,
-        bairro,
-        localidade,
-        uf,
-        ativo,
+      nome,
+      estado,
+      preco: parseFloat(preco),
     });
-
     const data = await hospede.criar();
 
     return res
@@ -68,6 +70,24 @@ export const criar = async (req, res) => {
       .json({ error: "Erro interno ao salvar o registro." });
   }
 };
+
+/**
+ * GET /api/hospedes
+ * @tags Hospedes
+ * @summary Busca todos os registros de hospedes
+ * @description EndPoint responsável por buscar hospedes cadastrados no sistema web.
+ * Permite filtrar os resultados utilizando parâmetros de consulta (query params).
+ *
+ * @param {string} nome.query
+ * @param {string} email.query
+ * @param {string} telefone.query
+ * @param {string} cep.query
+ * @param {boolean} ativo.query
+ * 
+ * @return {array<reqBodyHospede>} 200 - Lista de hóspedes encontrada com sucesso
+ * @return {object} 404 - Nenhum hóspede encontrado
+ * @return {object} 500 - Erro interno no servidor
+ */
 
 export const buscarTodos = async (req, res) => {
   try {
@@ -83,6 +103,19 @@ export const buscarTodos = async (req, res) => {
     return res.status(500).json({ error: "Erro ao buscar hospedes." });
   }
 };
+
+/**
+ * GET /api/hospedes/{id}
+ * @tags Hospedes
+ * @summary Busca um registro de hospede por ID
+ * @description EndPoint responsável por buscar um hospede específico cadastrado no sistema web a partir do ID.
+ * @param {integer} id.path.required
+ *
+  * @return 200 - Hospede encontrado com sucesso
+ * @return  400 - ID inválido
+ * @return  404 - Hóspede não encontrado
+ * @return  500 - Erro interno do servidor
+ */
 
 export const buscarPorId = async (req, res) => {
   try {
@@ -106,6 +139,21 @@ export const buscarPorId = async (req, res) => {
     return res.status(500).json({ error: "Erro ao buscar hospede." });
   }
 };
+
+/**
+ * PUT /api/hospedes/{id}
+ * @tags Hospedes
+ * @summary Atualiza um registro de hospede por ID
+ * @description Endpoint responsável por atualizar hospede específico pelo seu ID.
+ * @param {integer} id.path.required
+ * @param {reqBodyHospede} request.body.required
+ *
+ *
+ * @return 200 - ID do hospede encontrado
+ * @return 400 - Dados inválidos ou campos obrigatórios não informados
+ * @return 404 - Erro ao atualizar ID do registro
+ * @return 500 - Erro interno no servidor
+ */
 
 export const atualizar = async (req, res) => {
   try {
@@ -143,7 +191,7 @@ export const atualizar = async (req, res) => {
     return res
       .status(200)
       .json({
-        message: `O registro "${data.nome}" foi atualizado com sucesso!`,
+        message: O registro "${data.nome}" foi atualizado com sucesso!,
         data,
       });
   } catch (error) {
@@ -151,6 +199,19 @@ export const atualizar = async (req, res) => {
     return res.status(500).json({ error: "Erro ao atualizar hospede." });
   }
 };
+
+/**
+ * DELETE /api/hospedes/{id}
+ * @tags Hospedes
+ * @summary Deleta um registro de hospede por ID
+ * @description Endpoint responsável por deletar hospede específico pelo seu ID.
+ * @param {integer} id.path.required
+ *
+ * @return 200 - ID do hospede encontrado
+ * @return 400 - Dados inválidos ou campos obrigatórios não informados
+ * @return 404 - Erro ao deletar ID do registro
+ * @return 500 - Erro interno no servidor
+ */
 
 export const deletar = async (req, res) => {
   try {
@@ -173,7 +234,7 @@ export const deletar = async (req, res) => {
     return res
       .status(200)
       .json({
-        message: `O hospede "${hospede.nome}" foi deletado com sucesso!`,
+        message: O hospede "${hospede.nome}" foi deletado com sucesso!,
         deletado: hospede,
       });
   } catch (error) {
